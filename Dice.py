51c034e15ea126357c764a3e77d061d98d7779df
mysticalruby <90880156+mysticalruby@users.noreply.github.com>
2022-12-29 19:08:04 -0500

    Add files via upload

---
+++ Dice.py
@@ -0,0 +1,39 @@
+import random
+no = random.randint(1,6)
+if no==1:
+    print:("[     ]")
+    print:("[     ]")
+    print:("[  0  ]")
+    print:("[     ]")
+    print:("[     ]")
+if no==2:
+    print:("[     ]")
+    print:("[  0  ]")
+    print:("[     ]")
+    print:("[  0  ]")
+    print:("[     ]")
+if no==3:
+    print:("[0    ]")
+    print:("[     ]")
+    print:("[  0  ]")
+    print:("[     ]")
+    print:("[    0]")
+if no==4:
+    print:("[    0]")
+    print:("[0    ]")
+    print:("[     ]")
+    print:("[    0]")
+    print:("[0    ]")
+if no==5:
+    print:("[    0]")
+    print:("[0    ]")
+    print:("[  0  ]")
+    print:("[    0]")
+    print:("[0    ]")
+if no==6:
+    print:("[0    ]")
+    print:("[    0]")
+    print:("[0    ]")
+    print:("[    0]")
+    print:("[0    ]")
+    print:("[    0]")        
